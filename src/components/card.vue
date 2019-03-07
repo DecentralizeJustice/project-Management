90f936905505d3b73de9f48c61877510affa3675
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-03-07 01:49:38 -0800

    Added two to dos

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,17 +1,13 @@
 <template>
   <v-card class="mt-2" color=green lighten-5>
-     <v-img
-       src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
-       aspect-ratio="2.75"
-     ></v-img>
 
      <v-card-title primary-title class="justify-center">
        <div>
-         <h3 class="headline">Kangaroo Valley Safari</h3>
-         <div> by Donely </div>
+         <h3 class="headline">{{propObj.title}}</h3>
+
        </div>
      </v-card-title>
-
+     <div class="subheading"> by {{propObj.author}} </div>
      <v-card-actions class="justify-center">
        <v-btn flat color="primary">Explore</v-btn>
        <v-btn flat color="primary">Specification</v-btn>
@@ -23,10 +19,8 @@
 export default {
   name: 'card',
   props: {
-    // Info: {
-    //   type: Object,
-    //   required: true
-    // }
+
+    propObj: { type: Object, required: true }
   }
 }
 </script>
--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -12,7 +12,37 @@
          <v-divider light></v-divider>
          <v-layout row wrap class="justify-center">
            <v-flex xs10 mb-2 >
-             <card/>
+             <card :propObj="first"/>
+             <card :propObj="second"/>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-flex>
+      <v-flex md4 xs12>
+        <v-card dark >
+         <v-card-title primary-title class="justify-center" >
+           <div>
+             <div class="headline text-xs-center">Doing</div>
+           </div>
+         </v-card-title>
+         <v-divider light></v-divider>
+         <v-layout row wrap class="justify-center">
+           <v-flex xs10 mb-2 style="min-height:25vh;" >
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-flex>
+      <v-flex md4 xs12>
+        <v-card dark >
+         <v-card-title primary-title class="justify-center" >
+           <div>
+             <div class="headline text-xs-center">Under Review</div>
+           </div>
+         </v-card-title>
+         <v-divider light></v-divider>
+         <v-layout row wrap class="justify-center">
+           <v-flex xs10 mb-2 style="min-height:25vh;">
+
             </v-flex>
           </v-layout>
         </v-card>
@@ -31,7 +61,14 @@ export default {
     card
   },
   data: () => ({
-
+    first: {
+      author: 'Donely',
+      title: 'Build Deconstruction Visualization'
+    },
+    second: {
+      author: 'Donely',
+      title: 'Build Deconstruction Documentation'
+    }
   })
 }
 </script>
